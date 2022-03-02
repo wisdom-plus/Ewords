e1178348500f67955f3cedda7f4cb472e412361c
tomoron <tomoron326@outlook.jp>
2022-03-02 15:31:12 +0900

    add:home_router

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/dashboard' => 'home#dashboard'
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 end
