6a11520daaea76e5f08d98eae969c639d2a62167
tomoron <tomoron326@outlook.jp>
2022-03-03 21:43:16 +0900

    pref:devise_router

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -2,7 +2,13 @@
 #
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {
+    :confirmations => 'users/confirmations',
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions',
+    :passwords => 'users/passwords'
+  }
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
