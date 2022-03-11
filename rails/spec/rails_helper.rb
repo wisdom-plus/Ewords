bfc682e52920802004ade248623578e953bd4351
tomoron <tomoron326@outlook.jp>
2022-03-11 22:30:23 +0900

    add_config:module_include

--- rails/spec/rails_helper.rb
+++ rails/spec/rails_helper.rb
@@ -21,7 +21,7 @@ require 'capybara/rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
@@ -77,6 +77,7 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
   config.include Warden::Test::Helpers
   config.include Devise::Test::IntegrationHelpers, type: :request
+  config.include TurboStreamSpecSupport, type: :request
 end
 
 Capybara.register_driver :remote_chrome do |app|
