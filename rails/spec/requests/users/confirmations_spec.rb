4f076bdd4b9797cb5aef371ded327805f280b35d
tomoron <tomoron326@outlook.jp>
2022-03-12 22:17:21 +0900

    fix:rubocop

--- rails/spec/requests/users/confirmations_spec.rb
+++ rails/spec/requests/users/confirmations_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Confirmations', type: :request do
-  let(:user) {create(:user)}
+  let(:user) { create(:user) }
 
   describe 'GET /users/confirmation/new' do
     it 'リクエストが成功' do
@@ -10,13 +10,12 @@ RSpec.describe 'Confirmations', type: :request do
     end
   end
 
-
   describe 'GET /users/confirmation' do
     context 'success' do
       before do
-        @raw,enc = Devise.token_generator.generate(User, :confirmation_token)
+        @raw, enc = Devise.token_generator.generate(User, :confirmation_token)
         user.confirmation_token = enc
-        user.confirmation_sent_at = (Time.current - 4.hours)
+        user.confirmation_sent_at = (4.hours.ago)
         user.save(validate: false)
       end
 
@@ -28,28 +27,27 @@ RSpec.describe 'Confirmations', type: :request do
 
     context 'failure' do
       before do
-        @raw,enc = Devise.token_generator.generate(User, :confirmation_token)
+        @raw, enc = Devise.token_generator.generate(User, :confirmation_token)
         user.confirmation_token = enc
-        user.confirmation_sent_at = (Time.current - 1.hours)
+        user.confirmation_sent_at = (1.hour.ago)
         user.save(validate: false)
       end
 
       it 'リクエストが失敗(トークンが違う)' do
-        get user_confirmation_path(confirmation_token: 'another_token'),xhr: true
+        get user_confirmation_path(confirmation_token: 'another_token'), xhr: true
         expect(response).to have_http_status(:ok)
       end
     end
-
   end
 
   describe 'POST /users/confirmation' do
     it 'リクエストが成功' do
-      post user_confirmation_path, params: { user: {email: user.email}}
+      post user_confirmation_path, params: { user: { email: user.email } }
       expect(response).to have_http_status(:found)
     end
 
     it 'リクエストが失敗(登録されていないメールアドレス)' do
-      post user_confirmation_path, params: { user: {email: 'another@example.com'}},xhr: true
+      post user_confirmation_path, params: { user: { email: 'another@example.com' } }, xhr: true
       expect(response).to have_http_status(:ok)
     end
   end
--- rails/spec/requests/users/passwords_spec.rb
+++ rails/spec/requests/users/passwords_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Passwords', type: :request do
-  let(:user) { create(:user)}
+  let(:user) { create(:user) }
 
   describe 'GET /usrs/password/new' do
     it 'リクエストが成功' do
@@ -13,9 +13,9 @@ RSpec.describe 'Passwords', type: :request do
   describe 'GET /users/password/edit' do
     context 'success' do
       before do
-        @raw,enc = Devise.token_generator.generate(User, :reset_password_token)
+        @raw, enc = Devise.token_generator.generate(User, :reset_password_token)
         user.reset_password_token = enc
-        user.reset_password_sent_at = (Time.current - 4.hours)
+        user.reset_password_sent_at = (4.hours.ago)
         user.save(validate: false)
       end
 
@@ -35,7 +35,7 @@ RSpec.describe 'Passwords', type: :request do
 
   describe 'POST /users/password' do
     it 'リクエストが成功' do
-      post user_password_path, params: {user: {email: user.email}}
+      post user_password_path, params: { user: { email: user.email } }
       expect(response).to have_http_status(:found)
     end
   end
@@ -43,44 +43,43 @@ RSpec.describe 'Passwords', type: :request do
   describe 'PATCH /users/password' do
     context 'success' do
       before do
-        @raw,enc = Devise.token_generator.generate(User, :reset_password_token)
+        @raw, enc = Devise.token_generator.generate(User, :reset_password_token)
         user.reset_password_token = enc
-        user.reset_password_sent_at = (Time.current - 4.hours)
+        user.reset_password_sent_at = (4.hours.ago)
         user.save(validate: false)
       end
 
       it 'リクエストが成功' do
-        patch user_password_path, params:{ user: {reset_password_token: @raw,password:'another_password' ,password_confirmation: 'another_password'}}
+        patch user_password_path, params: { user: { reset_password_token: @raw, password: 'another_password', password_confirmation: 'another_password' } }
         expect(response).to have_http_status(:found)
       end
 
       it 'リクエストが成功(パスワードが6文字)' do
-        patch user_password_path, params:{ user: {reset_password_token: @raw,password:'passwo' ,password_confirmation: 'passwo'}}
+        patch user_password_path, params: { user: { reset_password_token: @raw, password: 'passwo', password_confirmation: 'passwo' } }
         expect(response).to have_http_status(:found)
       end
     end
 
     context 'failure' do
       before do
-        @raw,enc = Devise.token_generator.generate(User, :reset_password_token)
+        @raw, enc = Devise.token_generator.generate(User, :reset_password_token)
         user.reset_password_token = enc
-        user.reset_password_sent_at = (Time.current - 4.hours)
+        user.reset_password_sent_at = (4.hours.ago)
         user.save(validate: false)
       end
 
       it 'リクエストが失敗(トークンが違う)' do
-        patch user_password_path, params:{ user: {reset_password_token: 'another_token',password:'another_password' ,password_confirmation: 'another_password'}}
+        patch user_password_path, params: { user: { reset_password_token: 'another_token', password: 'another_password', password_confirmation: 'another_password' } }
         expect(response).to have_http_status(:ok)
       end
 
-
       it 'リクエストが失敗(パスワードが短い5文字)' do
-        patch user_password_path, params:{ user: {reset_password_token: @raw,password:'passw' ,password_confirmation: 'passw'}}
+        patch user_password_path, params: { user: { reset_password_token: @raw, password: 'passw', password_confirmation: 'passw' } }
         expect(response).to have_http_status(:ok)
       end
 
       it 'リクエストが失敗(パスワードが一致しない)' do
-        patch user_password_path, params:{ user: {reset_password_token: @raw,password:'another_password' ,password_confirmation: 'password'}}
+        patch user_password_path, params: { user: { reset_password_token: @raw, password: 'another_password', password_confirmation: 'password' } }
         expect(response).to have_http_status(:ok)
       end
     end
