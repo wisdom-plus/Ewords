9d792a82a92dda3f2bdbe2df9dd3990eb4152350
tomoron <tomoron326@outlook.jp>
2022-03-12 22:16:03 +0900

    test:user_confirmation_request

--- rails/spec/requests/users/confirmations_spec.rb
+++ rails/spec/requests/users/confirmations_spec.rb
@@ -12,10 +12,45 @@ RSpec.describe 'Confirmations', type: :request do
 
 
   describe 'GET /users/confirmation' do
+    context 'success' do
+      before do
+        @raw,enc = Devise.token_generator.generate(User, :confirmation_token)
+        user.confirmation_token = enc
+        user.confirmation_sent_at = (Time.current - 4.hours)
+        user.save(validate: false)
+      end
+
+      it 'リクエストが成功' do
+        get user_confirmation_path(confirmation_token: @raw)
+        expect(response).to have_http_status(:found)
+      end
+    end
+
+    context 'failure' do
+      before do
+        @raw,enc = Devise.token_generator.generate(User, :confirmation_token)
+        user.confirmation_token = enc
+        user.confirmation_sent_at = (Time.current - 1.hours)
+        user.save(validate: false)
+      end
+
+      it 'リクエストが失敗(トークンが違う)' do
+        get user_confirmation_path(confirmation_token: 'another_token'),xhr: true
+        expect(response).to have_http_status(:ok)
+      end
+    end
 
   end
 
   describe 'POST /users/confirmation' do
+    it 'リクエストが成功' do
+      post user_confirmation_path, params: { user: {email: user.email}}
+      expect(response).to have_http_status(:found)
+    end
 
+    it 'リクエストが失敗(登録されていないメールアドレス)' do
+      post user_confirmation_path, params: { user: {email: 'another@example.com'}},xhr: true
+      expect(response).to have_http_status(:ok)
+    end
   end
 end
