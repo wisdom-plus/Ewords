62e2f55645ef0fc45fa768684e8cb701a9ce2007
tomoron <tomoron326@outlook.jp>
2022-03-16 18:18:35 +0900

    fix:test_case_name

--- rails/spec/system/other_spec.rb
+++ rails/spec/system/other_spec.rb
@@ -5,25 +5,25 @@ RSpec.describe 'Other',type: :system,js: true do
 
   describe 'Flash message' do
 
-    it 'フラッシュメッセージが表示される(ログイン)' do
+    it 'フラッシュメッセージが表示される(notice)' do
       login(user)
       expect(page).to have_selector '#flash-message-notice', text: 'ログインしました。'
     end
 
-    it 'フラッシュメッセージが表示される(ログアウト)' do
+    it 'フラッシュメッセージが表示される(alert)' do
       login(user)
       click_on 'ログアウト'
       expect(page).to have_selector '#flash-message-notice', text: 'ログアウトしました。'
     end
 
-    it 'フラッシュメッセージが消せる(ログイン)' do
+    it 'フラッシュメッセージが消せる(notice)' do
       login(user)
       expect(page).to have_selector '#flash-message-notice', text: 'ログインしました。'
       find(".flash-delete-button").click
       expect(page).to have_no_selector '#flash-message-notice', text: 'ログインしました。'
     end
 
-    it 'フラッシュメッセージが消せる(ログアウト)' do
+    it 'フラッシュメッセージが消せる(alert)' do
       login(user)
       click_on 'ログアウト'
       expect(page).to have_selector '#flash-message-notice', text: 'ログアウトしました。'
