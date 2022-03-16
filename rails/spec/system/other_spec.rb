a2676e3cd80eae6bd9cbe9d9aae058181422e60c
tomoron <tomoron326@outlook.jp>
2022-03-16 18:21:01 +0900

    fix:rubocop

--- rails/spec/system/other_spec.rb
+++ rails/spec/system/other_spec.rb
@@ -1,10 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe 'Other',type: :system,js: true do
-  let(:user) { create(:user)}
+RSpec.describe 'Other', type: :system, js: true do
+  let(:user) { create(:user) }
 
   describe 'Flash message' do
-
     it 'フラッシュメッセージが表示される(notice)' do
       login(user)
       expect(page).to have_selector '#flash-message-notice', text: 'ログインしました。'
@@ -19,7 +18,7 @@ RSpec.describe 'Other',type: :system,js: true do
     it 'フラッシュメッセージが消せる(notice)' do
       login(user)
       expect(page).to have_selector '#flash-message-notice', text: 'ログインしました。'
-      find(".flash-delete-button").click
+      find('.flash-delete-button').click
       expect(page).to have_no_selector '#flash-message-notice', text: 'ログインしました。'
     end
 
@@ -27,7 +26,7 @@ RSpec.describe 'Other',type: :system,js: true do
       login(user)
       click_on 'ログアウト'
       expect(page).to have_selector '#flash-message-notice', text: 'ログアウトしました。'
-      find(".flash-delete-button").click
+      find('.flash-delete-button').click
       expect(page).to have_no_selector '#flash-message-notice', text: 'ログアウトしました。'
     end
   end
