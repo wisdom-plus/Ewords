7554d0fe4386d24b6f689d251bd12b8a64a8c2e4
tomoron <tomoron326@outlook.jp>
2022-03-16 23:24:21 +0900

    fix

--- rails/spec/system/other_spec.rb
+++ rails/spec/system/other_spec.rb
@@ -5,11 +5,7 @@ RSpec.describe 'Other', type: :system, js: true do
 
   describe 'Flash message' do
     it 'フラッシュメッセージが表示される(notice)' do
-      user.confirm
-      visit new_user_session_url
-      fill_in 'E-mail address', with: user.email
-      fill_in 'Password', with: user.password
-      click_button 'Log in'
+      login(user)
       expect(page).to have_selector '#flash-message-notice', text: 'ログインしました。'
     end
 
