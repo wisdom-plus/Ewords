267cfa5978354c045457e1606910a76fd86c4531
tomoron <tomoron326@outlook.jp>
2022-03-24 20:12:10 +0900

    test:policy_request

--- rails/spec/requests/home_spec.rb
+++ rails/spec/requests/home_spec.rb
@@ -7,4 +7,16 @@ RSpec.describe 'Homes', type: :request do
       expect(response).to have_http_status(:success)
     end
   end
+  describe 'GET /policy' do
+    it 'returns http success' do
+      get policy_path
+      expect(response).to have_http_status(:success)
+    end
+  end
+  describe 'GET /private_policy' do
+    it 'returns http success' do
+      get private_policy_path
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
