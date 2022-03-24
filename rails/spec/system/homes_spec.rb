8819fbd2182ad45d322d97d17b9dc0318aafe6c5
tomoron <tomoron326@outlook.jp>
2022-03-24 20:12:57 +0900

    fix:rubocop

--- rails/spec/system/homes_spec.rb
+++ rails/spec/system/homes_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe "Home", type: :system,js: true do
+RSpec.describe 'Home', type: :system, js: true do
   describe 'policy' do
     it '画面が表示される' do
       visit root_path
-      find(".setting_button").click
-      find(".policy_link").click
+      find('.setting_button').click
+      find('.policy_link').click
       expect(page).to have_current_path policy_path
     end
   end
@@ -13,8 +13,8 @@ RSpec.describe "Home", type: :system,js: true do
   describe 'private_policy' do
     it '画面が表示される' do
       visit root_path
-      find(".setting_button").click
-      find(".private_link").click
+      find('.setting_button').click
+      find('.private_link').click
       expect(page).to have_current_path private_policy_path
     end
   end
