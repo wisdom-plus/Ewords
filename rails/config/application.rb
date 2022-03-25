91dd735ed4377ae5dce907dfe801375490750792
tomoron <tomoron326@outlook.jp>
2022-03-25 21:21:42 +0900

    pref:spec_model

--- rails/config/application.rb
+++ rails/config/application.rb
@@ -43,7 +43,7 @@ module Ewords
       g.test_framework :rspec,
                        view_specs: false,
                        helper_specs: false,
-                       model_specs: false,
+                       models_specs: false,
                        routing_specs: false
     end
   end
