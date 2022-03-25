e22212df642f1e86d871f7d3abfa2a51e1d8306a
tomoron <tomoron326@outlook.jp>
2022-03-25 22:43:14 +0900

    add:enum_kind_and_level

---
+++ rails/app/models/word.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: words
+#
+#  id         :bigint           not null, primary key
+#  kind       :integer          default(0), not null
+#  level      :integer          default(0), not null
+#  reading    :text             not null
+#  word       :string(255)      not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Word < ApplicationRecord
+
+  enum level: {primer:0,basic: 1,abvance:2,abbreviation:3,initial:4}
+  enum kind: {nan:0, "名詞":1, "動詞":2,"副詞":3,"形容詞":4,"前置詞":5,"代名詞":6}
+end
