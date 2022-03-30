bb60e237f891b3cb1d01f82a70cdab1b96e11644
tomoron <tomoron326@outlook.jp>
2022-03-30 18:27:23 +0900

    add:show_controller

--- rails/app/controllers/words_controller.rb
+++ rails/app/controllers/words_controller.rb
@@ -3,6 +3,13 @@ class WordsController < ApplicationController
   def index;end
 
   def show
+    answer = Word.answer(show_params)
+    options =
+  end
 
+  private
+
+  def show_params
+    params.require(:word).permit(:level)
   end
 end
