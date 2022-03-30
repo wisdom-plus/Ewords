58b1b4de84395ce052f6470e7d4eb535e4ee7761
tomoron <tomoron326@outlook.jp>
2022-03-30 21:02:21 +0900

    test:request_word

--- rails/spec/requests/words_spec.rb
+++ rails/spec/requests/words_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 RSpec.describe 'Words', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    it 'レスポンス成功' do
+      get words_path
+      expect(response).to have_http_status(:ok)
+    end
   end
 end
