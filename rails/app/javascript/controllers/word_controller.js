11a96bd188bac6336f2146f1f6d3524bc6c57d23
tomoron <tomoron326@outlook.jp>
2022-04-08 21:24:41 +0900

    add_remove_hidden

--- rails/app/javascript/controllers/word_controller.js
+++ rails/app/javascript/controllers/word_controller.js
@@ -3,11 +3,19 @@ import { Controller } from "@hotwired/stimulus";
 // Connects to data-controller="word"
 export default class extends Controller {
   next(event) {
-    const option = event.currentTarget;
-    if (option.id === "correct") {
-      console.log("OK");
+    const click_option = event.currentTarget;
+    if (click_option.id === "correct") {
+      console.log(click_option);
     } else {
-      console.log("miss");
+      console.log(click_option);
     }
+    this.remove_hidden();
+  }
+
+  remove_hidden() {
+    const icon = document.querySelectorAll(".mark_icon");
+    icon.forEach(function (value) {
+      value.classList.remove("hidden");
+    });
   }
 }
