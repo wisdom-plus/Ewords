64d70c8a06d8e99d6a7213f6850c1bebc027c3a0
tomoron <tomoron326@outlook.jp>
2022-04-10 21:05:40 +0900

    add:session

--- rails/app/controllers/words_controller.rb
+++ rails/app/controllers/words_controller.rb
@@ -1,7 +1,21 @@
 class WordsController < ApplicationController
+  before_action :reset_session
+
   def show
-    @num = 0
     @answer = Word.answer(params[:level])
     @options = Word.choices(@answer, params[:level])
+    set_session(@answer.ids, 0)
+  end
+
+  private
+
+  def reset_session
+    session[:answer].clear
+    session.delete(:num)
+  end
+
+  def set_session(ids,num)
+    session[:answer] = ids
+    session[:num] = num
   end
 end
