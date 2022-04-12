19ec6affa659bf6c921370066260920fbd50d091
tomoron <tomoron326@outlook.jp>
2022-04-12 20:41:08 +0900

    pref:routes_add_num

--- rails/app/controllers/words_controller.rb
+++ rails/app/controllers/words_controller.rb
@@ -2,16 +2,20 @@ class WordsController < ApplicationController
   before_action :reset_session
 
   def show
-    @answer = Word.answer(params[:level])
-    @options = Word.choices(@answer, params[:level])
-    set_session(@answer.ids, 9)
+    if params[:num] == "0"
+      @answer = Word.answer(params[:level])
+      @options = Word.choices(@answer, params[:level])
+      set_session(@answer.ids, params[:num])
+    else
+      session[:num] = prams[:num]
+    end
   end
 
   private
 
   def reset_session
-    session[:answer].clear
     session.delete(:num)
+    session.delete(:answer)
   end
 
   def set_session(ids,num)
