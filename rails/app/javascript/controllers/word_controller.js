1e85961cbfb34fca6f4ad58f5b7dfae595cca22e
tomoron <tomoron326@outlook.jp>
2022-04-13 21:09:41 +0900

    add:second answer

--- rails/app/javascript/controllers/word_controller.js
+++ rails/app/javascript/controllers/word_controller.js
@@ -3,28 +3,35 @@ import { Controller } from "@hotwired/stimulus";
 // Connects to data-controller="word"
 export default class extends Controller {
   static targets = ["next"];
+  static values = { answered: Boolean };
 
   next(event) {
-    const click_option = event.currentTarget;
-    if (click_option.id === "correct") {
-      click_option.classList.remove(
-        "bg-white",
-        "hover:bg-gray-100",
-        "dark:border-gray-400",
-        "dark:hover:bg-gray-500"
-      );
-      click_option.classList.add("bg-green-200", "dark:bg-green-300");
-    } else {
-      click_option.classList.remove(
-        "bg-white",
-        "hover:bg-gray-100",
-        "dark:border-gray-400",
-        "dark:hover:bg-gray-500"
-      );
-      click_option.classList.add("bg-red-200", "dark:bg-red-300");
+    const answered = this.answeredValue;
+    if (!answered) {
+      const click_option = event.currentTarget;
+      if (click_option.id === "correct") {
+        click_option.classList.remove(
+          "bg-white",
+          "hover:bg-gray-100",
+          "dark:border-gray-400",
+          "dark:hover:bg-gray-500"
+        );
+        click_option.classList.add("bg-green-200", "dark:bg-green-300");
+        this.set_session(true);
+      } else {
+        click_option.classList.remove(
+          "bg-white",
+          "hover:bg-gray-100",
+          "dark:border-gray-400",
+          "dark:hover:bg-gray-500"
+        );
+        click_option.classList.add("bg-red-200", "dark:bg-red-300");
+        this.set_session(false);
+      }
+      this.remove_hidden();
+      this.next_button();
+      this.answeredValue = true;
     }
-    this.remove_hidden();
-    this.next_button();
   }
 
   remove_hidden() {
@@ -37,5 +44,18 @@ export default class extends Controller {
   next_button() {
     const next_button = this.nextTarget;
     next_button.classList.remove("hidden");
+  }
+
+  set_session(answer) {
+    const correct_answer = sessionStorage.getItem("correct_answer");
+    if (correct_answer == null) {
+      sessionStorage.setItem("correct_answer", answer);
+    } else {
+      const correct_answer_arr = correct_answer.split(",");
+      correct_answer_arr.push(answer);
+      const new_correct_answer = correct_answer_arr.join("");
+      sessionStorage.setItem("correct_answer", new_correct_answer);
+      console.log(new_correct_answer);
+    }
   }
 }
