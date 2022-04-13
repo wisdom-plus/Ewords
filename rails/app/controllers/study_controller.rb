acbba8623c88c2d281f32e0f3821d2933f930397
tomoron <tomoron326@outlook.jp>
2022-04-13 23:40:04 +0900

    add:create

--- rails/app/controllers/study_controller.rb
+++ rails/app/controllers/study_controller.rb
@@ -1,3 +1,8 @@
 class StudyController < ApplicationController
   def index; end
+
+  def create
+    session[:correct_answers].push(params[:answer])
+    redirect_to study_index_path,status: :see_other
+  end
 end
