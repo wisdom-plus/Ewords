549fdf27a6cad948330907547dec555cedf10546
tomoron <tomoron326@outlook.jp>
2022-04-19 19:06:55 +0900

    fix:answer sort ids

--- rails/app/controllers/study_controller.rb
+++ rails/app/controllers/study_controller.rb
@@ -1,26 +1,18 @@
 class StudyController < ApplicationController
   before_action :reset_session, if: :first_question?, only: :show
-  # after_action :reset_session,only: :result
+  before_action :redirect_result, only: :show
+
   def index; end
 
   def show
-    if finish?
-      session[:correct_answers].push(params[:answer])
-      redirect_to result_study_index_path, status: :see_other
-      return
-    elsif first_question?
-      ids = Word.answer_ids(params[:level])
-    else
-      ids = session[:answer_ids]
-      session[:correct_answers].push(params[:answer])
-    end
+    ids = answer_ids(params)
     @answer = Word.find(ids[params[:num].to_i])
     @options = Word.choices(@answer)
     set_session(ids, params[:num])
   end
 
   def result
-    @answer_record = Word.where(id: session[:answer_ids])
+    @answer_record = Word.where(id: session[:answer_ids]).in_order_of(:id, session[:answer_ids])
     @result = session[:correct_answers]
     @answer_count = @result.count { |answer| answer == 'true' }
   end
@@ -44,5 +36,22 @@ class StudyController < ApplicationController
     def set_session(ids, num)
       session[:answer_ids] = ids
       session[:num] = num
+    end
+
+    def redirect_result
+      return unless finish?
+
+      session[:correct_answers].push(params[:answer])
+      redirect_to result_study_index_path, status: :see_other
+    end
+
+    def answer_ids(params)
+      if first_question?
+        ids = Word.answer_ids(params[:level])
+      else
+        ids = session[:answer_ids]
+        session[:correct_answers].push(params[:answer])
+      end
+      ids
     end
 end
