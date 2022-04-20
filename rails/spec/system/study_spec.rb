4568dda388ddc47a2ad3e7be7abaf39c009fdf51
tomoron <tomoron326@outlook.jp>
2022-04-20 17:39:16 +0900

    test:system study index

--- rails/spec/system/study_spec.rb
+++ rails/spec/system/study_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe 'Study', type: :system, js: true do
+  describe 'index' do
+    it 'ボタンが表示される' do
+      visit study_index_path
+      expect(page).to have_link '学習する', href: word_study_index_path(level: 'primer',num: 0)
+      expect(page).to have_link '学習する', href: word_study_index_path(level: 'basic',num: 0)
+      expect(page).to have_link '学習する', href: word_study_index_path(level: 'abvance',num: 0)
+      expect(page).to have_link '学習する', href: word_study_index_path(level: 'abbreviation',num: 0)
+      expect(page).to have_link '学習する', href: word_study_index_path(level: 'initial',num: 0)
+    end
 
+  end
+
+  describe 'show' do
+
+  end
+
+  describe 'result' do
+
+  end
 end
