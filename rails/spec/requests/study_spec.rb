4b1790bb69653a90a18d59dc64ed6faab864cf34
tomoron <tomoron326@outlook.jp>
2022-04-20 14:59:32 +0900

    test:study request

--- rails/spec/requests/study_spec.rb
+++ rails/spec/requests/study_spec.rb
@@ -1,9 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe 'Studies', type: :request do
-  describe 'GET /index' do
+  let(:word) {create(:word)}
+  let(:word1) {create(:word,word: 'test1',reading: 'テスト1',level: 1,kind: 1)}
+
+  describe 'GET /study' do
     it 'returns http success' do
       get study_index_path
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'Get /words/level/:num' do
+    it 'returns http success' do
+      word
+      get word_study_index_path(num: 0,level: 'basic')
       expect(response).to have_http_status(:success)
     end
   end
